c2607e3d044d49ec6d61ee2e3ebf7c48c0636f0c
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-04-04 17:26:09 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    static int brown_row;
+    static int brown_col;
+    
+    public int[] solution(int brown, int yellow) {
+        
+        for (int i = 1;  i <= yellow; i++) {
+            int yellow_col = i;
+            int yellow_row = yellow / i;
+            brown_row = 0;
+            brown_col = 0;
+            
+            if (yellow_row >= yellow_col) {
+                if (yellow % yellow_row > 0) {
+                    yellow_col++;
+                }
+                
+                brown_row = yellow_row + 2;
+                brown_col = yellow_col + 2;
+                
+                if ((brown_row * brown_col) == (brown + yellow)) {
+                    break;
+                }
+            } else break;
+        }
+        
+        
+        
+        int[] answer = new int[2];
+        answer[0] = brown_row;
+        answer[1] = brown_col;
+        
+        return answer;
+    }
+}
