3b0ab33a57061980698c63c415777df0a1a15134
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-21 23:34:10 +0900

    [Bronze V] Title: A/B, Time: 132 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        double A = in.nextInt();
-        double B = in.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
         System.out.println(A / B);
-        
-        in.close();
     }
 }
