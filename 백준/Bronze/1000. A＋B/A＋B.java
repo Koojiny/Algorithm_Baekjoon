cd6961676a7d5538bb44e8b200220c9e17715d4c
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-27 07:25:54 +0900

    [Bronze V] Title: A+B, Time: 140 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        int A = in.nextInt();
-        int B = in.nextInt();
-        
-        System.out.println(A + B);
-        
-        in.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
     }
 }
