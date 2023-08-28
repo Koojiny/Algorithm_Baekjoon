31c5b65c1ff585fbfae91b4b1f325c2976dff3d4
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-28 22:42:12 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13836 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,7 @@
+import java.io.*;
+
 public class Main {
-    public static void main (String[] args) {
-        System.out.print("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
     }
 }
