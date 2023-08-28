ebb3ab206a129c38347cf9acdf3188e934821ff8
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-28 23:17:02 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        int[] arr = new int[b.length()];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = b.charAt(i) - '0';
+        }
+
+        System.out.println(Integer.parseInt(a) * arr[2]);
+        System.out.println(Integer.parseInt(a) * arr[1]);
+        System.out.println(Integer.parseInt(a) * arr[0]);
+        System.out.println((Integer.parseInt(a) * arr[0] * 100) + (Integer.parseInt(a) * arr[1]) * 10 + (Integer.parseInt(a) * arr[2]));
+    }
+}
