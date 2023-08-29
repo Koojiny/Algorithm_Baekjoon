1ee70edfb718748dab64b31e55f48e0b86e6b79c
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-29 17:29:16 +0900

    [Bronze V] Title: N 찍기, Time: 988 ms, Memory: 30732 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            int N = sc.nextInt();
-            for (int i = 1; i < N + 1; i++) {
-                System.out.println(i);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
         }
+    }
 }
