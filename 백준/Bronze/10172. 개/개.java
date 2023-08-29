deaa15c0fcced1f236dad69ccf3278bb2a84c85e
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-29 20:54:11 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,5 +1,8 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("|\\_/|");
         System.out.println("|q p|   /}");
         System.out.println("( 0 )\"\"\"\\");
