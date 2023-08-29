1c7fd99e2c520d5304cdaaf995bd7468de3ca8ba
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-29 20:53:59 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13880 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,5 +1,9 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
