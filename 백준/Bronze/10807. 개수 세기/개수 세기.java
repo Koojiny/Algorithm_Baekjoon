67df7a312f0ba9be90a8edaeeee9e336cb9376ce
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-08-30 22:41:40 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int v = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == v) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
