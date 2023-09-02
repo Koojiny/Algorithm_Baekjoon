5ac82b5444887deaef9edcf117542917d0b32d57
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-09-02 23:59:14 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,21 +1,16 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String args[]) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
         String str;
 
-        while( (str=br.readLine()) != null ){
-
-            st = new StringTokenizer(str," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a + b).append("\n");
-
+        while ((str = br.readLine()) != null) {
+            int a = str.charAt(0) - 48;
+            int b = str.charAt(2) - 48;
+            sb.append(a + b).append('\n');
         }
         System.out.print(sb);
     }
