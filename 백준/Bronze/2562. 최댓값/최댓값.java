eea76fecb1fdbd0176778b37509419e09ea86460
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-09-03 21:18:02 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -3,18 +3,15 @@ import java.io.*;
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
 
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        
         int max = 0;
         int idx = 0;
-        
+
         for (int i = 0; i < 9; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
+            int val = Integer.parseInt(br.readLine());
+
+            if (val > max) {
+                max = val;
                 idx = i;
             }
         }
