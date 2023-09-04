0f4217303f30e29bc2ef47e1888a51cf70e55257
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-09-04 17:26:44 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,24 +1,18 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean[] student = new boolean[30];
-        int[] result = new int[30];
 
         for (int i = 0; i < 28; i++) {
             student[Integer.parseInt(br.readLine()) - 1] = true;
         }
-        
+
         for (int i = 0; i < 30; i++) {
             if (student[i] == false) {
-                result[i] = i + 1;
+                System.out.println(i + 1);
             }
         }
-        Arrays.sort(result);
-        
-        System.out.println(result[28]);
-        System.out.println(result[29]);
     }
 }
