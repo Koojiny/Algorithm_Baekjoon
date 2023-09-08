f7398760f1659058b364437bad21192bcfccc2dd
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-09-08 22:57:41 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            sb.append(solution(br.readLine())).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static String solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+}
