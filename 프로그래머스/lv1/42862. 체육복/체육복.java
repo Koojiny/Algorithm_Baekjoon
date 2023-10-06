29b4544d70b530a6f72ea621b6438daf2ae62275
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-10-06 16:59:35 +0900

    [level 1] Title: 체육복, Time: 0.06 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+
+        // 1. Set을 만든다
+        HashSet<Integer> resSet = new HashSet<>();
+        HashSet<Integer> lostSet = new HashSet<>();
+
+        for (int i : reserve) {
+            resSet.add(i);
+        }
+
+        for (int i : lost) {
+            if (resSet.contains(i)) { // 여분이 있는 학생이 잃어버렸는지 확인
+                resSet.remove(i);
+            } else {
+                lostSet.add(i);
+            }
+        }
+
+        // 2. 여분을 기준으로 앞뒤로 확인하여 체육복을 빌려준다
+        for (int i : resSet) {
+            if (lostSet.contains(i - 1)) {
+                lostSet.remove(i - 1);
+            } else if (lostSet.contains(i + 1)) {
+                lostSet.remove(i + 1);
+            }
+        }
+        
+        // 3. 전체 학생 수에서 lostSet에 남은 학생수를 빼준다
+        return n - lostSet.size();
+    }
+}
