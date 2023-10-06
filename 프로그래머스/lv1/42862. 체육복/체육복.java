fac9e368eb41735a0cb4b8d79ebed402ae5f81e7
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-10-06 17:11:37 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -1,34 +1,38 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
+    public static int solution(int n, int[] lost, int[] reserve) {
+        // 1. student 배열 생성
+        int[] student = new int[n + 2];
+        int answer = 0;
 
-        // 1. Set을 만든다
-        HashSet<Integer> resSet = new HashSet<>();
-        HashSet<Integer> lostSet = new HashSet<>();
-
-        for (int i : reserve) {
-            resSet.add(i);
+        // 2. reserve / lost 정보 반영
+        for (int l : lost) {
+            student[l]--;
+        }
+        for (int r : reserve) {
+            student[r]++;
         }
 
-        for (int i : lost) {
-            if (resSet.contains(i)) { // 여분이 있는 학생이 잃어버렸는지 확인
-                resSet.remove(i);
-            } else {
-                lostSet.add(i);
+        // 3. 여분을 기준으로 앞뒤를 확인하여 체육복을 빌려준다
+        for (int i = 1; i <= n; i++) {
+            if (student[i] == 1) {
+                if (student[i - 1] == -1) {
+                    student[i]--;
+                    student[i - 1]++;
+                } else if (student[i + 1] == -1) {
+                    student[i]--;
+                    student[i + 1]++;
+                }
             }
         }
 
-        // 2. 여분을 기준으로 앞뒤로 확인하여 체육복을 빌려준다
-        for (int i : resSet) {
-            if (lostSet.contains(i - 1)) {
-                lostSet.remove(i - 1);
-            } else if (lostSet.contains(i + 1)) {
-                lostSet.remove(i + 1);
+        // 4. 체육복을 갖고 있는 학생수를 계산한다
+        for (int i = 1; i <= n; i++) {
+            if (student[i] >= 0) {
+                answer++;
             }
         }
-        
-        // 3. 전체 학생 수에서 lostSet에 남은 학생수를 빼준다
-        return n - lostSet.size();
+        return answer;
     }
 }
