e50c38d646c74d281ea89912a423301cabb76428
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-10-13 23:46:28 +0900

    [Bronze II] Title: 소수 찾기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,7 +16,7 @@ public class Main {
     }
 
     public static boolean isPrime(int n) {
-        if (n ==1) return false;
+        if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
         }
