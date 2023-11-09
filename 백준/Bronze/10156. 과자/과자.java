43e7e5e965b1950101a101763ceb8b55c7701f15
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2023-11-09 22:04:15 +0900

    [Bronze IV] Title: 과자, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int res = K * N - M;
+        if (res > 0) System.out.println(res);
+        else System.out.println(0);
+    }
+}
