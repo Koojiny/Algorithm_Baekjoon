a8b6158ab90ea421376727a60d4a38ae7fb3c0bb
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-16 21:46:13 +0900

    [level 2] Title: 소수 찾기, Time: 34.78 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,41 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static HashSet<Integer> set = new HashSet<>();
+    public static HashSet<Integer> numberSet = new HashSet<>(); // 숫자 조합 넣을 해시셋 선언;
     
     public int solution(String numbers) {
         int answer = 0;
         
-        // 1. 모든 숫자의 조합을 만든다
-        combination("", numbers);
+        // 1. 모든 조합의 숫자를 생성
+        makeNumber("", numbers);
         
-        // 2. 소수인지 판별한다
-        Iterator<Integer> it = set.iterator();
-        
-        while(it.hasNext()) {
-            int num = it.next();
-            if (isPrime(num)) answer++;
+        System.out.println(numberSet);
+        // 2. 소수 판별
+        Iterator<Integer> it = numberSet.iterator();
+        while (it.hasNext()) {
+            int number = it.next();
+            if (isPrime(number)) answer++;
         }
-        
-        // 3. 소수 개수 반환
+            
+        // 3. 소수 개수 리턴
         return answer;
     }
     
-    public static void combination(String comb, String forUse) {
-        if (!comb.equals("")) { // 빈 문자열이 아니라면 값을 Set에 넣어라
-            set.add(Integer.parseInt(comb));
-        }
+    public void makeNumber(String comb, String others) {
+        // 1. 현재 조합을 set에 추가
+        if (!comb.equals(""))
+            numberSet.add(Integer.valueOf(comb));
         
-        for (int i = 0; i < forUse.length(); i++) {
-            combination(comb + forUse.charAt(i), forUse.substring(0, i) + forUse.substring(i + 1));
+        // 2. 남은 숫자 중 한 개를 더해 새로운 조합을 만든다
+        for (int i = 0; i < others.length(); i++) {
+            makeNumber(comb + others.charAt(i), others.substring(0, i) + others.substring(i + 1));
         }
     }
     
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
+        
         return true;
     }
 }
