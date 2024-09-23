3b11d30614edb42b523224e457fd06f1fd43d230
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-23 23:03:28 +0900

    [level 2] Title: 모음 사전, Time: 37.96 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 3.89 ms
+메모리: 76 MB, 시간: 37.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 1일 11:18:36
+2024년 09월 23일 23:03:27
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,39 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static String[] arr;
-    static List<String> list;
+    
+    public static List<String> list;
+    public static String[] words = {"A", "E", "I", "O", "U"};
     
     public int solution(String word) {
-        
-        arr = new String[]{"A", "E", "I", "O", "U"};
+        int answer = 0;
         list = new ArrayList<>();
         
-        // 재귀함수 호출
-        recursion("", 0);
+        dfs("", 0);
         
-        Iterator<String> it = list.iterator();
-        int count = 0;
-        
-        while (it.hasNext()) {
-            String cur = it.next();
-            if (cur.equals(word)) break;
-            count++;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
         }
         
-        // 몇 번째인지 순서 반환
-        return count;
+        return answer;
     }
     
-    static void recursion(String str, int depth) {
-        list.add(str);
+    public static void dfs(String s, int len) {
+        list.add(s);
         
-        // 종료조건: 재귀함수 내에서 깊이 5가 되면 return
-        if (depth == 5) return;
+        if (len == 5) return;
         
-        // 수행동작: 재귀함수로 단어를 모두 생성
-        for (int i = 0; i < arr.length; i++) {
-            recursion(str + arr[i], depth + 1);
+        for (int i = 0; i < 5; i++) {
+            dfs(s + words[i], len + 1);
         }
     }
 }
