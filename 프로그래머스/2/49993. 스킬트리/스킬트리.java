cf037a5836de313044e0b2dc68df54ac0a8ff7c3
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-09-23 22:20:17 +0900

    [level 2] Title: 스킬트리, Time: 8.35 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        
+        for (String cur : skill_trees) { // skill_trees의 문자열 하나씩 탐색
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < cur.length(); i++) {	
+                if (skill.contains(cur.charAt(i) + "")) { // 문자 순서대로 skill에 포함되는것만
+                    sb.append(cur.charAt(i));
+                }
+            }
+            
+            answer += skill.indexOf(sb.toString()) == 0 ? 1 : 0;
+        }
+        return answer;
+    }
+}
