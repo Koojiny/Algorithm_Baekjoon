669dc5e0388fce1120743ca99d74eade17bcb427
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2024-10-21 22:09:13 +0900

    [Silver I] Title: 그림, Time: 384 ms, Memory: 45196 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -3,20 +3,25 @@ import java.util.*;
 
 public class Main {
 
-    static int n, m, cnt, max;
-    static int[][] board;
-    static boolean[][] isVis;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy= {0, 0, -1, 1};
+    public static boolean[][] visit;
+    public static int[][] board;
+    public static Queue<int[]> q = new LinkedList<>();
+    public static int[] dy = new int[]{0, 0, -1, 1};
+    public static int[] dx = new int[]{-1, 1, 0, 0};
+    public static int maxSize, count; // 가장 넓은 것의 넓이, 그림 개수
+    public static int n, m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken()); // 세로 크기
-        m = Integer.parseInt(st.nextToken()); // 가로 크기
+        n = Integer.parseInt(st.nextToken()); // 세로
+        m = Integer.parseInt(st.nextToken()); // 가로
 
-        board = new int[n][m]; // 도화지 배열
+        board = new int[n][m];
+        visit = new boolean[n][m];
+
+        // 보드 초기화
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -24,44 +29,41 @@ public class Main {
             }
         }
 
-        isVis = new boolean[n][m]; // 방문확인 배열
-
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 1 && !isVis[i][j]) { // 그림이 그려져 있거나 방문하지 않은 경우 BFS
-                    max = Math.max(max, BFS(i, j));
-                    cnt++;
+            for(int j = 0; j < m; j++) {
+                if (board[i][j] == 1 && !visit[i][j]) {
+                    bfs(i, j);
+                    count++;
                 }
             }
         }
-        System.out.println(cnt);
-        System.out.println(max);
+
+        System.out.println(count);
+        System.out.println(maxSize);
     }
 
-    static int BFS(int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
-        isVis[x][y] = true;
+    private static void bfs(int y, int x) {
         int size = 1;
+        q.offer(new int[]{y, x});
+        visit[y][x] = true;
 
         while (!q.isEmpty()) {
-            int[] p = q.poll();
-            int px = p[0];
-            int py = p[1];
+            int[] cur = q.poll();
+            int cy = cur[0];
+            int cx = cur[1];
 
-            for (int d = 0; d < 4; d++) {
-                int nx = px + dx[d];
-                int ny = py + dy[d];
+            for (int k = 0; k < 4; k++) {
+                int ny = cy + dy[k];
+                int nx = cx + dx[k];
 
-                if (nx < 0 || ny < 0 || ny > m - 1 || nx > n - 1) continue;
-                if (isVis[nx][ny]) continue;
-                if (board[nx][ny] == 1) {
+                if (0 <= ny && ny < n && 0 <= nx && nx < m && !visit[ny][nx] && board[ny][nx] == 1) {
+                    q.offer(new int[]{ny, nx});
+                    visit[ny][nx] = true;
                     size++;
-                    isVis[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
                 }
             }
         }
-        return size;
+
+        if (size > maxSize) maxSize = size;
     }
 }
