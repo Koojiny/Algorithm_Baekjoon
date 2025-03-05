4000ced79c9cee4dd1dd0897a7ca10e069eb7beb
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-03-05 09:56:07 +0900

    [level 2] Title: 스킬트리, Time: 18.28 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 73.7 MB, 시간: 8.35 ms
+메모리: 72.7 MB, 시간: 18.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 23일 22:20:14
+2025년 03월 05일 09:56:04
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -4,17 +4,20 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
-        for (String cur : skill_trees) { // skill_trees의 문자열 하나씩 탐색
+        // 1. 스킬트리 배열 순회
+        for (String s : skill_trees) {
+            // 2. 스킬트리 하나를 꺼낸 후 각 문자열을 선행스킬과 비교하여 스킬트리 한 개를 필터링
             StringBuilder sb = new StringBuilder();
             
-            for (int i = 0; i < cur.length(); i++) {	
-                if (skill.contains(cur.charAt(i) + "")) { // 문자 순서대로 skill에 포함되는것만
-                    sb.append(cur.charAt(i));
+            for (int i = 0; i < s.length(); i++) {
+                if (skill.contains(s.charAt(i) + "")) { // 선행스킬 내에 없는 스킬인 경우 sb에 추가
+                    sb.append(s.charAt(i));
                 }
             }
-            
-            answer += skill.indexOf(sb.toString()) == 0 ? 1 : 0;
+            // 3. 최종적으로 필터링 된 스킬트리를 다시 순회하면서 선행스킬에 포함된 문자열인지 확인 후 Count + 1
+            if (skill.startsWith(sb.toString())) answer++;
         }
+       
         return answer;
     }
 }
