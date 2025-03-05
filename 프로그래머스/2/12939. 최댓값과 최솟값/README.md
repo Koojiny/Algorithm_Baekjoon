2ff7543de8010f9b8af92db5e9e5d813130e892f
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-03-05 18:07:13 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.70 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 16.57 ms
+메모리: 88.9 MB, 시간: 9.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 20:07:31
+2025년 03월 05일 18:07:09
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
         StringTokenizer st = new StringTokenizer(s);
+        
         int n = st.countTokens();
         int[] arr = new int[n];
         
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
         Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
         
-        answer = arr[0] + " " + arr[arr.length - 1];
-        return answer;
+        return arr[0] + " " + arr[arr.length - 1];
     }
 }
