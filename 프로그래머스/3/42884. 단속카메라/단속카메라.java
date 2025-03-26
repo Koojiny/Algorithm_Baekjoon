775ac35da71791b804d3d504af6b34e0feef51e1
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-03-26 11:36:05 +0900

    [level 3] Title: 단속카메라, Time: 12.09 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,25 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        // 정렬을 진행
+        // 두 번째 값 기준 오름차순, 같으면 첫 번째 값 기준 오름차순
         Arrays.sort(routes, new Comparator<int[]>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
+            public int compare(int[] a, int[] b) {
+                if (a[1] != b[1]) {
+                    return Integer.compare(a[1], b[1]);
+                } else {
+                    return Integer.compare(a[0], b[0]);
+                }
             }
         });
-        // System.out.println(Arrays.deepToString(routes));
         
-        int answer = 0;
-        int curCamera = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < routes.length; i++) {
-            
-            if (routes[i][0] > curCamera) {
-                curCamera = routes[i][1];
+        int std = routes[0][1];
+        int answer = 1;
+        for (int i = 1; i < routes.length; i++) {
+            // System.out.println(Arrays.toString(pair));
+            if (routes[i][0] <= std) continue;
+            else {
                 answer++;
+                std = routes[i][1];
             }
         }
+        
         return answer;
     }
 }
