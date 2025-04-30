6918d04de12c31773258e3973b2c1ecb0376fe44
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-04-30 10:46:54 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 900 ms, Memory: 57068 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -7,21 +7,21 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Map<String, Integer> map = new HashMap<>();
-        String[] pArr = new String[N + 1];
+        Map<String, Integer> hash1 = new HashMap<>();
+        Map<Integer, String> hash2 = new HashMap<>();
 
         for (int i = 1; i < N + 1; i++) {
             String pokemon = br.readLine();
-            map.put(pokemon, i);
-            pArr[i] = pokemon;
+            hash1.put(pokemon, i);
+            hash2.put(i, pokemon);
         }
 
         for (int i = 0; i < M; i++) {
             String s = br.readLine();
-            if (map.containsKey(s)) {
-                System.out.println(map.get(s));
+            if (hash1.containsKey(s)) {
+                System.out.println(hash1.get(s));
             } else {
-                System.out.println(pArr[Integer.parseInt(s)]);
+                System.out.println(hash2.get(Integer.parseInt(s)));
             }
         }
     }
