c9af802d2cfff2b87685adc9f4a9b9f86e0c58c7
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-02 23:29:15 +0900

    [level 2] Title: 점 찍기, Time: 15.16 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        
+        for (long x = 0; x <= d; x += k) {
+            // long y = (long) Math.sqrt(d * d - x * x);
+            answer += (int) Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2)) / k + 1;
+        }
+        
+        return answer;
+    }
+}
