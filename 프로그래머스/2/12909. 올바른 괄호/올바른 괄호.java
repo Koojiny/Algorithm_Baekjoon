94d8ad4bea3270ca620bc87064b6bf789d490e79
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 14:12:25 +0900

    [level 2] Title: 올바른 괄호, Time: 19.05 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stk = new Stack<>();
         
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            } else {
-                if (stack.size() < 1) return false;
-                if (stack.peek() == '(') {
-                    stack.pop();
-                } else {
-                    stack.push(s.charAt(i));
-                }
+            if (s.charAt(i) == '(') stk.push(s.charAt(i));
+            else {
+                if (stk.size() > 0) stk.pop();
+                else return false;
             }
         }
+        
+        if (stk.size() == 0) return true;
+        else return false;
 
-        return stack.size()  < 1 ? true : false;
     }
 }
