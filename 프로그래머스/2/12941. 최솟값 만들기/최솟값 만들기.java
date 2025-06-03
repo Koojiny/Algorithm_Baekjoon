c18060cccb02151c6ecf6a0c050a3dfef3cfd817
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 14:25:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.76 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -7,11 +7,10 @@ class Solution
         int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
-        
         for (int i = 0; i < A.length; i++) {
-            answer += A[i] * B[B.length - i - 1];
+            answer += A[i] * B[B.length - 1 - i];
         }
-        
+
         return answer;
     }
 }
