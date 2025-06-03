3bbadfca9463616d92fe42055acb580ace144cf7
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 16:03:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 84.55 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,23 +4,20 @@ class Solution
 {
     public int solution(String s)
     {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stk = new Stack<>();
         
-        for (int i = 0; i < s.length(); i++) { // 문자열 순회
-            if (stack.isEmpty()) { // 스택이 비어있는 경우에는 현재 알파벳 삽입
-                stack.push(s.charAt(i));
-            } else {
-                char c = s.charAt(i); // 현재 알파벳
-                
-                if (stack.peek() == c) {
-                    stack.pop();
+        for (int i = 0; i < s.length(); i++) {
+            if (stk.size() == 0) stk.push(s.charAt(i));
+            else if (stk.size() > 0) {
+                char tmp = stk.peek();
+                if (tmp == s.charAt(i)) {
+                    stk.pop();
                 } else {
-                    stack.push(s.charAt(i));
+                    stk.push(s.charAt(i));
                 }
             }
         }
-        
-        // 문자열 순회가 끝난 후에도 스택이 비어있지 않다면 문자 제거 실패
-        return stack.isEmpty() ? 1 : 0;
+
+        return stk.size() == 0? 1 : 0;
     }
 }
