3d6104ed0a697dd44c6904c5fb785a339ada9592
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 15:16:14 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 44.53 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,44 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
-        
+        int count = 0;
         int zeroCnt = 0;
-        int cnt = 0;
-        // long num = Integer.parseInt(s);
         
         while (s.length() > 1) {
-            StringBuilder sb = new StringBuilder();
-            
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '0') zeroCnt++;
-                else sb.append(s.charAt(i));
             }
-            System.out.println(sb);
-            s = Integer.toBinaryString(sb.toString().length());
-
-            // num = cal(sb.toString().length());
-            // s = String.valueOf(num);
-            cnt++;
+            s = s.replaceAll("0", "");
+            s = Integer.toBinaryString(s.length());
+            count++;
         }
         
-        System.out.println(cnt);
-        System.out.println(zeroCnt);
-        answer[0] = cnt;
+        answer[0] = count;
         answer[1] = zeroCnt;
-        return answer;
-    }
-    
-    public int cal(int num) { 
-        int temp = num;
-        StringBuilder sb = new StringBuilder();
-        while (temp > 0) {
-            temp /= 2;
-            sb.append(num % 2);
-            num = temp;
-        }
         
-        String tempStr = sb.reverse().toString();
-
-        return Integer.parseInt(tempStr);
+        return answer;
     }
 }
