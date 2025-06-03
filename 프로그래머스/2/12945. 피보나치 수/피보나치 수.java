f621bdd52e96226964941779bd34d403398a5652
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 16:16:21 +0900

    [level 2] Title: 피보나치 수, Time: 3.74 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int[] answer = new int[n + 1];
-        answer[0] = 0;
-        answer[1] = 1;
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
         
-        for (int i = 2; i <= n; i++) {
-            answer[i] = (answer[i - 2] + answer[i - 1]) % 1234567;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1234567;
         }
-        
-        //System.out.println(Arrays.toString(answer));
-        
-        return answer[n];
+    
+        return arr[n];
     }
 }
