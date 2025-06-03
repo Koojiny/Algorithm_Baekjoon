82a7bf0cfa8e81f3bed616213b1c0828a10fcec2
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 14:47:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 13.17 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String[] words = s.split(" ", -1); // 공백 포함하여 분리
-        StringBuilder answer = new StringBuilder();
-        
+        String[] words = s.split(" ", -1);
+        StringBuilder sb = new StringBuilder();
+        // System.out.println(Arrays.toString(words));
         for (int i = 0; i < words.length; i++) {
             if (words[i].length() > 0) {
                 words[i] = Character.toUpperCase(words[i].charAt(0)) + words[i].substring(1).toLowerCase();
             }
-            answer.append(words[i]);
-            if (i < words.length - 1) {
-                answer.append(" "); // 단어 사이 공백 유지
-            }
+            sb.append(words[i]);
+            if (i < words.length - 1) sb.append(" ");
         }
-        
-        return answer.toString();
+        return sb.toString();
     }
 }
