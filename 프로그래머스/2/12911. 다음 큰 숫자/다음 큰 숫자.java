921282acf3776b313d96ac6df0ff3ad76fa93bea
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-06-03 15:48:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,30 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int tmpNum = n;
-        int cntOne = countOne(n);
-        
-        while (true) {
-            tmpNum++;
-            int tmpCntOne = countOne(tmpNum);
-            if (tmpCntOne == cntOne) break;
-        }
-        
-        return tmpNum;
-    }
-    
-    public static int countOne(int num) {
-        String s = Integer.toBinaryString(num);
-        int cntOne = 0;
-        
+        int temp = n + 1;
+        int zeroCnt = 0;
+        String s = Integer.toBinaryString(n);
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == '1') {
-                cntOne++;
+            if (s.charAt(i) == '1') zeroCnt++;
+        }
+
+        while (true) {
+            int zeroTemp = 0;
+            String tempNum = Integer.toBinaryString(temp);
+
+            for (int j = 0; j < tempNum.length(); j++) {
+                if (tempNum.charAt(j) == '1') zeroTemp++;
             }
+            
+            if (zeroCnt == zeroTemp) break;
+            temp++;
         }
         
-        return cntOne;
+        return temp;
     }
 }
