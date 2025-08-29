8bdaf4a64484c432de334934c6b1861292eb5b2a
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-08-29 16:38:04 +0900

    [Gold IV] Title: 알고스팟, Time: 132 ms, Memory: 15296 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -3,12 +3,9 @@ import java.util.*;
 
 public class Main {
     public static int n, m;
-    public static int[][] miro;
-    public static int x, y; // 현재 좌표
-    public static int brokenWall;
-    public static boolean[][] visited;
-    public static int[] dx = {-1, 0, 1, 0};
-    public static int[] dy = {0, 1, 0, -1};
+    public static int[][] miro, dist;
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {1, 0, -1, 0};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,68 +13,49 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         miro = new int[n][m];
-        visited = new boolean[n][m];
+        dist = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < m; j++) {
                 miro[i][j] = Integer.parseInt(str.substring(j, j + 1));
-            }
-        }
-        x = 0;
-        y = 0;
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(x, y, 0));
-        visited[x][y] = true;
-
-        int nx, ny;
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
-
-            if (cur.x == n - 1 && cur.y == m - 1) {
-                brokenWall = cur.cnt;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                nx = cur.x + dx[i];
-                ny = cur.y + dy[i];
-
-                if (!inRange(nx, ny)) continue;
-
-                if (!visited[nx][ny] && miro[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    pq.offer(new Node(nx, ny, cur.cnt));
-                }
-
-                if (!visited[nx][ny] && miro[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    pq.offer(new Node(nx, ny, cur.cnt + 1));
-                }
+                dist[i][j] = -1;
             }
         }
 
-        System.out.println(brokenWall);
+        bfs();
+
+        System.out.println(dist[n - 1][m - 1]);
+    }
+
+    public static void bfs() {
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.add(new int[]{0, 0});
+        dist[0][0] = 0;
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = curX + dx[dir];
+                int ny = curY + dy[dir];
+
+                if (inRange(nx, ny) && dist[nx][ny] == -1) {
+                    if (miro[nx][ny] == 0) {
+                        dq.addFirst(new int[]{nx, ny});
+                        dist[nx][ny] = dist[curX][curY];
+                    } else {
+                        dq.addLast(new int[]{nx, ny});
+                        dist[nx][ny] = dist[curX][curY] + 1;
+                    }
+                }
+            }
+        }
     }
 
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < m;
-    }
-}
-
-class Node implements Comparable<Node> {
-    int x;
-    int y;
-    int cnt;
-
-    public Node(int x, int y, int cnt) {
-        this.x = x;
-        this.y = y;
-        this.cnt = cnt;
-    }
-
-    @Override
-    public int compareTo(Node other) {
-        return this.cnt - other.cnt;
     }
 }
