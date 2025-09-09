04ffce174ef9c1d852469973db7c0e94c77b0441
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-09-09 23:05:04 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[][] dp = new int[31][31];
+        for (int i = 0; i <= 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[M][N]);
+        }
+    }
+}
