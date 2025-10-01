a117451fab15da78fa5282b20e490dfe3343b097
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-10-02 00:25:14 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 560 ms, Memory: 50804 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(a, b, c));
+        }
+
+        Collections.sort(edges);
+
+        parent = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        int totalWeight = 0;
+        for (Edge edge : edges) {
+            if (find(edge.u) != find(edge.v)) {
+                union(edge.u, edge.v);
+                totalWeight += edge.w;
+            }
+        }
+
+        System.out.println(totalWeight);
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parent[rootB] = rootA;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int u;
+        int v;
+        int w;
+
+        public Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge other) {
+            return this.w - other.w;
+        }
+    }
+}
