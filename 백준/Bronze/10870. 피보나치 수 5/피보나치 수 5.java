92366f2b2ac0182c48b785fe7917928d0feeb7be
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-10-08 22:50:54 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -5,18 +5,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
+        
         if (n == 0) {
             System.out.println(0);
             return;
         }
-
+        
         int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 2] + dp[i - 1];
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         System.out.println(dp[n]);
