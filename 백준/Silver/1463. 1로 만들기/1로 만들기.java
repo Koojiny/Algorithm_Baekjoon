e13dee6739409e0369779daf6454aedcf69f9fbf
Seokjin Moon <88961874+Koojiny@users.noreply.github.com>
2025-10-09 00:18:18 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18172 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 3];
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+        for (int i = 4; i <= n; i++) {
+            // 1 뺀 경우
+            dp[i] = dp[i - 1] + 1;
+            // 2로 나누어 떨어지면 나누기
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            // 3으로 나누어 떨어지면 나누기
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
